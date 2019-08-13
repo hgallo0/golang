4e85721b3feb15c3dbe5e2dca2a07a53de33fe53
henry Gallo <henrygallo@henrys-MacBook-Pro.local>
2019-08-13 14:28:35 -0400

    finished maps

--- from_book/ArraysSlicesandMaps/main.go
+++ from_book/ArraysSlicesandMaps/main.go
@@ -14,5 +14,26 @@ func main() {
   fmt.Println(cheeses)
 
   //slices
+  var cheeses_s = make([]string, 2)
+  cheeses_s[0] = "American"
+  cheeses_s[1] = "Mozzarella"
 
+  cheeses_s = append(cheeses_s, "Camembert")
+
+  fmt.Println(cheeses_s)
+
+  //maps
+  var players = make(map[string]int)
+
+  players["cook"] = 32
+
+  players["bairstow"] = 27
+
+  players["stokes"] = 26
+
+  fmt.Println(players)
+  fmt.Println(players["cook"])
+
+  delete(players, "cook")
+  fmt.Println(players)
 }
