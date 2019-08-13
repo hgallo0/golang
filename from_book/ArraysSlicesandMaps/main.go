0b506692fd77b7b1a7507ef5bf28cdc79cc89dbd
henry Gallo <henrygallo@henrys-MacBook-Pro.local>
2019-08-13 14:19:56 -0400

    adding more content

---
+++ from_book/ArraysSlicesandMaps/main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+  "fmt"
+)
+
+func main() {
+  //arrays
+  var cheeses [2]string
+
+  cheeses[0] = "American"
+  cheeses[1] = "Mozzarella"
+
+  fmt.Println(cheeses)
+
+  //slices
+
+}
---
+++ from_book/functions/main.go
@@ -0,0 +1,38 @@
+package main
+
+import "fmt"
+
+func addUp(x int, y int) int {
+  return x + y
+}
+
+func anotherFunction(f func() string) string {
+  return f()
+}
+
+func main () {
+  //basic function
+  fmt.Printf("%v\n", addUp(1,5))
+
+  //passing function as values
+  fn := func() {
+    fmt.Println("function called")
+  }
+
+
+
+  fn()
+
+  // passing function as argument
+  afn := func() string {
+    return "function call"
+  }
+
+  fmt.Println(anotherFunction(afn))
+
+}
+
+
+//engine light the purge valve sillinoed
+
+//203 8821971 mel gomez, part of the emition
